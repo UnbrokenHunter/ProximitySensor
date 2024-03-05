de6fce0a35bf1f994369b8de8e7d7e09b22d3715
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2024-03-05 18:23:06 -0500

    Added Support for Times Beyond 24 Hours

--- Globals.py
+++ Globals.py
@@ -4,6 +4,9 @@ def FormatTime(time):
     # Extract whole seconds and milliseconds
     seconds = int(time)
     milliseconds = int((time - seconds) * 1000)
+
+    # Compute Days
+    days = seconds // (24 * 3600)
     
     # Compute hours, minutes, and seconds
     seconds = seconds % (24 * 3600)
@@ -11,10 +14,19 @@ def FormatTime(time):
     seconds %= 3600
     minutes = seconds // 60
     seconds %= 60
-    
-    # Return formatted time string including milliseconds
-    return "%d:%02d:%02d.%03d" % (hour, minutes, seconds, milliseconds)
 
+    # Return including days
+    if days != 0:
+        return "%d:%d:%02d:%02d.%03d" % (days, hour, minutes, seconds, milliseconds)
+    
+    # Return including hours
+    elif hour != 0:
+        return "%d:%02d:%02d.%03d" % (hour, minutes, seconds, milliseconds)
+    
+    # Return including hours
+    else:
+        return "%02d:%02d.%03d" % (minutes, seconds, milliseconds)
+    
 TrackingEnabled = True
 LapFilters = True
 MinLapTime = 10
@@ -22,6 +34,7 @@ MinLapTime = 10
 CurrentDriver = "None"
 Drivers = ["Mac", "Mitchell", "Josh", "Hunter", "John Walter"]
 LapCount = 0
+CurrentLapTime = 0
 LastLapTime = 0
 
 # Statics
