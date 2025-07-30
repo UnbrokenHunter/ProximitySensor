73e6ae758388b90334f7b02e010f29426c46e549
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2025-07-30 13:04:47 -0400

    feat: Add Restart Sensor Button

--- src/tracker/ui/TitleWindow.py
+++ src/tracker/ui/TitleWindow.py
@@ -2,42 +2,14 @@ import customtkinter as tk
 import time
 
 from .. import Globals
+from .. import ProccessSensorData
 from ..utils import TimeUtils
 from ..sheets import Sheets, LocalSheets
 
-
 class Frame(tk.CTkFrame):     
-    
-    manualDriverInput = "None"
-
-    def LapFilters(self):
-        Globals.LapFilters = not Globals.LapFilters
-        print("Lap Filters: ", Globals.LapFilters)
-
-    def EnableTracking(self):
-        if (Globals.ManualTimeSet == True):
-            self.startTime.configure(text=TimeUtils.ConvertToFormattedDate(Globals.StartTime))
-            self.startTimeUnix.configure(text=Globals.StartTime)
-        elif (Globals.RealStart == False):
-            Globals.RealStart = True
-            Globals.StartTime = time.time()
-            self.startTime.configure(text=time.strftime("%Y-%m-%d %H:%M:%S"))
-            self.startTimeUnix.configure(text=Globals.StartTime)
-            
-        Globals.TrackingEnabled = not Globals.TrackingEnabled
-        print("Enable Tracking: ", Globals.TrackingEnabled)
-
-    def SetDriver(self, choice):
-        Globals.CurrentDriver = choice
-        print("Driver: ", choice)
-
-    def SetManualDriver(self, choice):
-        self.manualDriverInput = choice
-        print("Manual Driver Set:", choice)
-
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        
+
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
@@ -55,20 +27,54 @@ class Frame(tk.CTkFrame):
 
         self.driverLabel = tk.CTkLabel(master=self, text="Current Driver: ", font=("Helvetica", 20))
         self.driverLabel.grid(row=1, column=0, padx=10, pady=(10, 3), sticky="ew")
-        
-        self.driver = tk.CTkComboBox(master=self, 
-                                     values=Globals.Drivers,
-                                     command=self.SetDriver)
+
+        self.driver = tk.CTkComboBox(
+            master=self, 
+            values=Globals.Drivers,
+            command=lambda choice: (
+                setattr(Globals, "CurrentDriver", choice),
+                print("Driver:", choice)
+            )
+        )
         self.driver.set("None") 
         self.driver.grid(row=2, column=0, padx=10, pady=(3, 10), sticky="ew")
 
-        self.enableTracking = tk.CTkCheckBox(master=self, text="Enable Tracking", command=self.EnableTracking, height=50, font=("Helvetica", 20))
+        self.enableTracking = tk.CTkCheckBox(
+            master=self,
+            text="Enable Tracking",
+            command=self.toggle_tracking,
+            height=50,
+            font=("Helvetica", 20)
+        )
         self.enableTracking.deselect()
         self.enableTracking.grid(row=3, column=0, padx=10, pady=3, sticky="ew")
 
-        self.lapFilters = tk.CTkCheckBox(master=self, text="Lap Filters", command=self.LapFilters, height=50, font=("Helvetica", 20))
+        self.lapFilters = tk.CTkCheckBox(
+            master=self,
+            text="Lap Filters",
+            command=lambda: (
+                setattr(Globals, "LapFilters", not Globals.LapFilters),
+                print("Lap Filters:", Globals.LapFilters)
+            ),
+            height=50,
+            font=("Helvetica", 20)
+        )
         self.lapFilters.select()
         self.lapFilters.grid(row=4, column=0, padx=10, pady=3, sticky="ew")
-        
+
+        self.restartSensorButton = tk.CTkButton(self, text="Restart Sensor", command=ProccessSensorData.RestartSensor)
+        self.restartSensorButton.grid(row=5, column=0, padx=10, pady=3, sticky="ew")
 
 
+    def toggle_tracking(self):
+        if Globals.ManualTimeSet:
+            self.startTime.configure(text=TimeUtils.ConvertToFormattedDate(Globals.StartTime))
+            self.startTimeUnix.configure(text=Globals.StartTime)
+        elif not Globals.RealStart:
+            Globals.RealStart = True
+            Globals.StartTime = time.time()
+            self.startTime.configure(text=time.strftime("%Y-%m-%d %H:%M:%S"))
+            self.startTimeUnix.configure(text=Globals.StartTime)
+
+        Globals.TrackingEnabled = not Globals.TrackingEnabled
+        print("Enable Tracking:", Globals.TrackingEnabled)
